49e95bfdfc1bd803253ef60480663fdfe044fe92
Charles Pascoe <cpascoe95@gmail.com>
2015-10-06 11:18:28 +0100

    Added RsaPrivateKey

---
+++ src/main/java/uk/co/cpascoe/rsa/RsaPrivateKey.java
@@ -0,0 +1,32 @@
+package uk.co.cpascoe.rsa;
+
+import java.math.BigInteger;
+import java.util.Random;
+
+public class RsaPrivateKey {
+    private BigInteger p;
+    private BigInteger q;
+    private BigInteger n;
+    private BigInteger phi_n;
+    private BigInteger e;
+    private BigInteger d;
+
+    public RsaPrivateKey(int bits) {
+        this.e = new BitIngeger("65537");
+
+        Random r = new Random();
+
+        do {
+            this.p = new BigInteger(bit / 2, 15, r);
+            this.q = new BigInteger(bit / 2, 15, r);
+
+            this.n = this.p.multiply(q);
+            this.phi_n = this.p.subtract(BigInteger.ONE).multiply(this.q.subtract(BigInteger.ONE));
+
+        } while (!this.phi_n.gcd(this.e).equals(BigInteger.ONE));
+
+        this.d = this.e.modInverse(this.phi_n);
+    }
+
+
+}
