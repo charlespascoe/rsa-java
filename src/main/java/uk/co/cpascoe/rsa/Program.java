6b2e9d0298493e45179fb5249147f693a9a85a14
Charles Pascoe <cpascoe95@gmail.com>
2015-10-13 09:54:42 +0100

    Added some example code to print key values

--- src/main/java/uk/co/cpascoe/rsa/Program.java
+++ src/main/java/uk/co/cpascoe/rsa/Program.java
@@ -1,8 +1,14 @@
 package uk.co.cpascoe.rsa;
 
+import java.util.Map;
 
 public class Program {
     public static void main(String[] args) {
+        RsaPrivateKey key = new RsaPrivateKey(2048);
+        Map<String, String> data = key.exportToMap();
 
+        for (Map.Entry<String, String> entry : data.entrySet()) {
+            System.out.printf("%s: %s%n%n", entry.getKey(), entry.getValue());
+        }
     }
 }
