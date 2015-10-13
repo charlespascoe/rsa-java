088c25771c21ef97f980d72ec40e08bef13d0551
Charles Pascoe <cpascoe95@gmail.com>
2015-10-13 10:44:53 +0100

    Added RsaKey from Map<String, String> constructor

--- src/main/java/uk/co/cpascoe/rsa/RsaKey.java
+++ src/main/java/uk/co/cpascoe/rsa/RsaKey.java
@@ -8,6 +8,23 @@ public class RsaKey {
     protected BigInteger n;
     protected BigInteger e;
 
+    public RsaKey() {
+
+    }
+
+    public RsaKey(Map<String, String> data) throws Exception {
+        if (!data.containsKey("n") || !data.containsKey("e")) {
+            throw new Exception("Keys missing from data object");
+        }
+
+        if (!Utils.isValidHex(data.get("n")) || !Utils.isValidHex(data.get("e"))) {
+            throw new Exception("Invalid hex values");
+        }
+
+        this.n = new BigInteger(data.get("n"), 16);
+        this.e = new BigInteger(data.get("e"), 16);
+    }
+
     public BigInteger publicExponentation(BigInteger val) {
         return val.modPow(this.e, this.n);
     }
