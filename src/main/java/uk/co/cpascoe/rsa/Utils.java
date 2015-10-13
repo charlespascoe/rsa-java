268385e7b7ee38f2d9d990ca57e487c10db912c7
Charles Pascoe <cpascoe95@gmail.com>
2015-10-13 10:50:56 +0100

    Fixed Utils.isValidHex(String)

--- src/main/java/uk/co/cpascoe/rsa/Utils.java
+++ src/main/java/uk/co/cpascoe/rsa/Utils.java
@@ -4,7 +4,7 @@ import java.util.regex.Pattern;
 
 public abstract class Utils {
     public static boolean isValidHex(String str) {
-        Pattern p = new Pattern("^([0-9A-Fa-f]{2})+$");
+        Pattern p = Pattern.compile("^([0-9A-Fa-f]{2})+$");
         return p.matcher(str).matches();
     }
 }
