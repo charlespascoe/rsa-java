f198f14049d2fb351e46f05418f2d8056679618d
Charles Pascoe <cpascoe95@gmail.com>
2015-10-13 10:26:21 +0100

    Added Utils class, with isValidHex(String)

---
+++ src/main/java/uk/co/cpascoe/rsa/Utils.java
@@ -0,0 +1,10 @@
+package uk.co.cpascoe.rsa;
+
+import java.util.regex.Pattern;
+
+public abstract class Utils {
+    public static boolean isValidHex(String str) {
+        Pattern p = new Pattern("^([0-9A-Fa-f]{2})+$");
+        return p.matcher(str).matches();
+    }
+}
