1aa18c0630e0f43e6ea49fb05bfe8b6835af46ae
Charles Pascoe <cpascoe95@gmail.com>
2015-10-20 10:43:02 +0100

    Added MaskGeneratorTests.shouldBeDeterministic()

--- src/test/java/uk/co/cpascoe/rsa/MaskGeneratorTests.java
+++ src/test/java/uk/co/cpascoe/rsa/MaskGeneratorTests.java
@@ -3,6 +3,7 @@ package uk.co.cpascoe.rsa;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 
 public class MaskGeneratorTests {
 
@@ -46,6 +47,19 @@ public class MaskGeneratorTests {
         for (int i = 0; i <= 128; i++) {
             assertEquals(mg.generateMask(new byte[0], i).length, i);
         }
+    }
+
+    @Test
+    public void shouldBeDeterministic() throws NoSuchAlgorithmException {
+        MaskGenerator mg = this.createMaskGen();
+
+        byte[] seed1 = new byte[] {1, 2, 3, 4, 5, 6, 7, 8};
+        byte[] seed2 = new byte[] {8, 7, 6, 5, 4, 3, 2, 1};
+
+        assertArrayEquals(mg.generateMask(seed1, 128), mg.generateMask(seed1, 128));
+        assertArrayEquals(mg.generateMask(seed2, 128), mg.generateMask(seed2, 128));
+        assertFalse(Arrays.equals(mg.generateMask(seed1, 128), mg.generateMask(seed2, 128)));
+
     }
 
 }
