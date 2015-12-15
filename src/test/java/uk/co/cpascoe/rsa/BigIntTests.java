6781fd76a5c4ef0830571409442a68d76aa43b44
Charles Pascoe <cpascoe95@gmail.com>
2015-12-15 17:41:42 +0000

    Cleaned up createFromByteArray BigInt test

--- src/test/java/uk/co/cpascoe/rsa/BigIntTests.java
+++ src/test/java/uk/co/cpascoe/rsa/BigIntTests.java
@@ -16,10 +16,9 @@ public class BigIntTests {
         assertArrayEquals(new int[] {0}, new BigInt(new byte[] {0}).exportToIntArray());
         assertArrayEquals(new int[] {1}, new BigInt(new byte[] {1,0}).exportToIntArray());
         assertArrayEquals(new int[] {256}, new BigInt(new byte[] {0,1}).exportToIntArray());
-        // N.B.: Signed -1 int is the same as 4294967295 unsigned int
-        assertArrayEquals(new int[] {-1}, new BigInt(new byte[] {(byte)255,(byte)255,(byte)255,(byte)255}).exportToIntArray());
+        assertArrayEquals(new int[] {(int)4294967295L}, new BigInt(new byte[] {(byte)255,(byte)255,(byte)255,(byte)255}).exportToIntArray());
         assertArrayEquals(new int[] {0,1}, new BigInt(new byte[] {0,0,0,0,1}).exportToIntArray());
-        assertArrayEquals(new int[] {0,-1}, new BigInt(new byte[] {0,0,0,0,(byte)255,(byte)255,(byte)255,(byte)255}).exportToIntArray());
+        assertArrayEquals(new int[] {0,(int)4294967295L}, new BigInt(new byte[] {0,0,0,0,(byte)255,(byte)255,(byte)255,(byte)255}).exportToIntArray());
         assertArrayEquals(new int[] {1,2,3,4}, new BigInt(new byte[] {1,0,0,0,2,0,0,0,3,0,0,0,4,0,0,0}).exportToIntArray());
     }
 
