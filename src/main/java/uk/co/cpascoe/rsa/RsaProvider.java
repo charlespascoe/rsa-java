cc2652da9cb4e9d9d7ca755b4a13adc10f97fad2
Charles Pascoe <cpascoe95@gmail.com>
2015-12-31 12:36:31 +0000

    Added RsaProvider.encryptKey and tests

--- src/main/java/uk/co/cpascoe/rsa/RsaProvider.java
+++ src/main/java/uk/co/cpascoe/rsa/RsaProvider.java
@@ -1,12 +1,36 @@
 package uk.co.cpascoe.rsa;
 
+import java.util.Arrays;
 
 public class RsaProvider {
-    private RsaKey key;
+    private final RsaKey rsaKey;
 
-    public RsaProvider(RsaKey key) {
-        if (key == null) throw new NullPointerException("key should not be null");
-        this.key = key;
+    public RsaProvider(RsaKey rsaKey) {
+        if (rsaKey == null) throw new NullPointerException("rsaKey should not be null");
+        this.rsaKey = rsaKey;
+    }
+
+    protected byte[] encryptKey(byte[] symmetricKey) throws EncryptionException {
+        int keyByteLength = this.rsaKey.byteCount();
+
+        OaepProvider oaep = new OaepProvider();
+
+        if (symmetricKey.length > oaep.maxMessageLength(keyByteLength)) {
+            throw new EncryptionException("Key too long");
+        }
+
+        byte[] encodedKeyBlock;
+
+        try {
+            encodedKeyBlock = oaep.encode(symmetricKey, keyByteLength);
+        } catch (OaepProvider.EncodingException ex) {
+            throw new EncryptionException(ex);
+        }
+
+        BigInt m = new BigInt(encodedKeyBlock);
+        BigInt c = this.rsaKey.publicExponentation(m);
+
+        return Arrays.copyOf(c.exportToByteArray(), keyByteLength);
     }
 
     public byte[] encrypt(byte[] data) {
